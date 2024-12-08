6857c695c9b544bf93f8f5aeb289dcaf4a0e5382
valiente57 <142689349+valiente57@users.noreply.github.com>
2024-12-08 12:31:57 +0530

    Create Mint_&_Burn_ERC20.sol

---
+++ Mint_&_Burn_ERC20.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20_Tokens is ERC20, ERC20Burnable, Ownable {
+
+    constructor() ERC20("ASTRIAL", "SOUL") Ownable(msg.sender) {}
+
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+       _mint(to, amount); 
+    }
+
+    function transferTokens(address _receiver, uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        transferFrom(msg.sender, _receiver, amount);
+    }
+
+    function burnTokens(uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender,amount);
+    }
+}
